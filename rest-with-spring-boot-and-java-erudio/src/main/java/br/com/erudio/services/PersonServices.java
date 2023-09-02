3ade955ced73b930def54fb75970a8392e8272ed
Kaue Sousa <kauelimasousa02@gmail.com>
2023-09-01 23:22:19 -0300

    Implementos dos metodos Put, Post e Delete do Endpoint Person

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,20 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		
+		logger.info("Find all people!");
+		List<Person> persons = new ArrayList<>();
+		
+		for(int i =0; i< 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+		
+	}
+
 	public Person findById(String id) {
 		
 		logger.info("Find one person!");
@@ -25,5 +41,32 @@ public class PersonServices {
 		person.setGender("Male");
 		
 		return person;
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one person!");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String i) {
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {
+		Person person =  new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFistName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+		
+		return person;
+		
 	}
 }
